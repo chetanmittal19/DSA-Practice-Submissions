2676ebba48eebf548139c5464778b21d1679a51e
Chetan Mittal <107939336+chetanmittal19@users.noreply.github.com>
2023-08-15 18:45:40 +0530

    Time: 8 ms (97.84%) | Memory: 11.5 MB (44.19%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
@@ -11,18 +11,49 @@
 class Solution {
 public:
     ListNode* reverseKGroup(ListNode* head, int k) {
-        if(head==NULL) return head;
+        //Method 1 [Recursive] O(2N) = O(N), O(N)[For recursion stack]
+        // here we first check if the given list contains atleast k elemnts or not. 
+        // If we have k elements then we simply reverse those k elements and call recursive function for the 
+        // remaining elements else we simply return the head
+        // if(head==NULL || k==1) return head;
+        // ListNode *curr = head;
+        // for(int i=1; i<k; i++){
+        //     if(curr->next==NULL) return head;
+        //     curr = curr->next;
+        // }
+        // ListNode *remHead = curr->next;
+        // curr->next = NULL;
+        // reverse(head);
+        // head->next=reverseKGroup(remHead, k);
+        // return curr;
 
-        ListNode *curr = head;
-        for(int i=1; i<k; i++){
-            if(curr->next==NULL) return head;
-            curr = curr->next;
+        //Method 2 [Iterative] O(N), O(1)
+        if(head==NULL || k==1) return head;
+        ListNode *dummy = new ListNode(0);
+        dummy->next=head;
+        ListNode *prev = dummy, *curr = dummy, *nex = dummy;
+        int count = 0; //stores length of array
+        while(curr->next!=NULL){ 
+            curr=curr->next;
+            count++;
         }
-        ListNode *remHead = curr->next;
-        curr->next = NULL;
-        reverse(head);
-        head->next=reverseKGroup(remHead, k);
-        return curr;
+
+        while(count>=k){
+            curr=prev->next;
+            nex = curr->next;
+            //here we are reversing in pairs one by one, instead of reversing entire k elements at once
+            for(int i=1; i<k; i++){ //if we have to reverse k nodes then our reverse will always be k-1 times
+                curr->next = nex->next;
+                nex->next = prev->next;
+                prev->next = nex;
+                nex=curr->next;
+            }
+            prev=curr;
+            count-=k;
+        }
+        return dummy->next;
+        
+
     }
 
     void reverse(ListNode *head){
