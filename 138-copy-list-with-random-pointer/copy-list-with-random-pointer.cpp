c1cd0c943a5acdc25eb7d4098a39fa226bc8d25e
Chetan Mittal <107939336+chetanmittal19@users.noreply.github.com>
2023-08-16 01:10:21 +0530

    Time: 3 ms (97.41%) | Memory: 11.2 MB (87.28%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -17,22 +17,51 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
+        //Method 1 [HashMap] O(2N)=O(N), O(N)
+        // if(head==NULL) return head;
+        // unordered_map<Node*, Node*> map;
+        // Node *curr = head;
+        // while(curr!=NULL){
+        //     map[curr] = new Node(curr->val);
+        //     curr=curr->next;
+        // }
+        // curr=head;
+        // Node *cloneHead=map[curr];
+        // while(curr!=NULL){
+        //     Node *cloneNode = map[curr];
+        //     cloneNode->next = map[curr->next];
+        //     cloneNode->random = map[curr->random];
+        //     curr=curr->next;
+        // }
+        // return cloneHead;
+
+        //Method 2 
         if(head==NULL) return head;
-        unordered_map<Node*, Node*> map;
         Node *curr = head;
         while(curr!=NULL){
-            map[curr] = new Node(curr->val);
-            curr=curr->next;
+            Node *newNode = new Node(curr->val);
+            newNode->next = curr->next;
+            curr->next = newNode;
+            curr=newNode->next;
         }
-        curr=head;
-        Node *cloneHead=map[curr];
+
+        curr = head;
         while(curr!=NULL){
-            Node *cloneNode = map[curr];
-            cloneNode->next = map[curr->next];
-            cloneNode->random = map[curr->random];
-            curr=curr->next;
+            if(curr->random!=NULL) curr->next->random = curr->random->next;
+            curr=curr->next->next;
         }
-        return cloneHead;
+
+        curr=head;
+        Node *clonedHead = curr->next;
+        Node *clonedTail = clonedHead;
+        while(curr->next->next!=NULL){
+            curr->next = curr->next->next;
+            curr=curr->next;
+            clonedTail->next = clonedTail->next->next;
+            clonedTail = clonedTail->next;
+        }
+        curr->next = NULL;
+        return clonedHead;
 
     }
 };
