7a3220f0792e9f2dc815db85fe9c963f2adcf672
Chetan Mittal <107939336+chetanmittal19@users.noreply.github.com>
2023-08-16 17:03:20 +0530

    Time: 3 ms (69.58%) | Memory: 6.7 MB (80.72%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,24 +1,42 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        if(s.size()==0) return 0;
+        // Method 1 [Iterative] O(N), O(1)
+        // if(s.size()==0) return 0;
 
-        int i=0;
+        // int i=0;
+        // while(i<s.size() && s[i]==' ') i++;
+        // s = s.substr(i);
+
+        // int sign=1;
+        // if(s[0]=='-') sign = -1;
+        // i = (s[0]=='+' || s[0]=='-') ? 1 : 0;
+
+        // long ans=0;
+        // while(i<s.size()){
+        //     if(s[i]==' ' || !isdigit(s[i])) break;
+        //     ans = ans*10 + (s[i]-'0');
+        //     if(sign==-1 && -1 * ans < INT_MIN) return INT_MIN;
+        //     if(sign==1 && ans > INT_MAX) return INT_MAX;
+        //     i++;
+        // }
+        // return (int)(sign * ans);
+
+        // Method 2 [Recursive] O(N), O(N)
+        int i=0, n=s.size(), sign=1;
         while(i<s.size() && s[i]==' ') i++;
-        s = s.substr(i);
-
-        int sign=1;
-        if(s[0]=='-') sign = -1;
-        i = (s[0]=='+' || s[0]=='-') ? 1 : 0;
-
-        long ans=0;
-        while(i<s.size()){
-            if(s[i]==' ' || !isdigit(s[i])) break;
-            ans = ans*10 + (s[i]-'0');
-            if(sign==-1 && -1 * ans < INT_MIN) return INT_MIN;
-            if(sign==1 && ans > INT_MAX) return INT_MAX;
+        if(s[i]=='+') i++;
+        else if(s[i]=='-'){
             i++;
+            sign=-1;
         }
-        return (int)(sign * ans);
+        return recursiveAtoi(s, i, sign, 0);
+    }
+
+    int recursiveAtoi(string s, int i, int sign, long ans){
+        if(sign*ans>INT_MAX) return INT_MAX;
+        if(sign*ans<INT_MIN) return INT_MIN;
+        if(i>=s.size() || !isdigit(s[i])) return sign*ans;
+        return recursiveAtoi(s, i+1, sign, ans*10 + (s[i]-'0'));
     }
 };
