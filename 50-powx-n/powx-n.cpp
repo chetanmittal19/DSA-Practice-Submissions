27227771482dc2eccc3ab328f40cc2e424edcd53
Chetan Mittal <107939336+chetanmittal19@users.noreply.github.com>
2023-08-16 18:46:13 +0530

    Time: 3 ms (39.18%) | Memory: 6.1 MB (27.29%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        //Method 1 [Iterative]
-        double ans = 1.0;
-        long long nn = n;
-        if(nn<0) nn*=-1;
-        while(nn>0){
-            if(nn%2==1) ans*=x;
-            x*=x;
-            nn/=2;
-        }
-        if(n<0) ans=(double)(1.0)/(double)(ans);
-        return ans;
-
-        //Method 2 [Recursive]
-        // if(n==0) return 1;
-        
-        // double ans = myPow(x, n/2);
-        // ans*=ans;
-        // if(n%2==1) ans*=x;
+        //Method 1 [Iterative] O(logN), O(1) where N is no. of bits in 'n'
+        // double ans = 1.0;
+        // long long nn = n;
+        // if(nn<0) nn*=-1;
+        // while(nn>0){
+        //     if(nn%2==1) ans*=x;
+        //     x*=x;
+        //     nn/=2;
+        // }
+        // if(n<0) ans=(double)(1.0)/(double)(ans);
         // return ans;
+
+        //Method 2 [Recursive] O(logN), O(N)
+        return recursive(x, 1L * n);
+    }
+
+    double recursive(double x, long n){ //here we changed the datatype of n to long to prevent integer overflow
+        if(n==0) return 1;
+        if(n<0) return recursive(1/x, -n);
+        return (n % 2 == 0) ? recursive(x*x, n/2) : x * recursive(x*x, n/2);
     }
 };
