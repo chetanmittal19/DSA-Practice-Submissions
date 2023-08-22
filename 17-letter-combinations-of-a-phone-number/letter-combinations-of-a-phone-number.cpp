06cc52de44650e81416a5a6b3470bd40c2484dbe
Chetan Mittal <107939336+chetanmittal19@users.noreply.github.com>
2023-08-22 19:20:04 +0530

    Time: 3 ms (38.89%) | Memory: 6.5 MB (44.02%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,15 +1,13 @@
-// #include<vector>
-
 class Solution {
 public:
 
-    void recursive(string digits, vector<string> &ans, map<char,string> &hash, int count, string s){
+    void recursive(string digits, vector<string> &ans, string hash[], int count, string s){
         if(count==digits.size()){
             ans.push_back(s);
             return;
         }
 
-        string chcode = hash[digits[count]];
+        string chcode = hash[digits[count]-'2'];
         for(int i=0; i<chcode.size(); i++){
             recursive(digits, ans, hash, count+1, s+chcode[i]);
         }
@@ -18,16 +16,7 @@ public:
         vector<string> ans;
         if(digits.size()==0) return ans;
 
-        // string hash[]  = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-        map<char, string> hash;
-        hash['2'] = "abc";
-        hash['3'] = "def";
-        hash['4'] = "ghi";
-        hash['5'] = "jkl";
-        hash['6'] = "mno";
-        hash['7'] = "pqrs";
-        hash['8'] = "tuv";
-        hash['9'] = "wxyz";
+        string hash[]  = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
         recursive(digits, ans, hash, 0, "");
         return ans;
     }
