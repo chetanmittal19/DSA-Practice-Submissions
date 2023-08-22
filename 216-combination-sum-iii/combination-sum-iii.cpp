0f861f9a261bd8287d788fc8d7e7d29d68d0f601
Chetan Mittal <107939336+chetanmittal19@users.noreply.github.com>
2023-08-22 17:54:34 +0530

    Time: 0 ms (100.00%) | Memory: 6.5 MB (53.84%) - LeetSync

---
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int>> ans;
+        vector<int> v;
+        recursive(k, n, ans, v, 1, 0);
+        return ans;
+    }
+
+    void recursive(int k, int n, vector<vector<int>> &ans, vector<int> &v, int ind, int sum){
+        if(sum>n) return;
+        if(sum==n && k==0){
+            ans.push_back(v);
+            return;
+        }
+
+        for(int i=ind; i<=9; i++){
+            v.push_back(i);
+            recursive(k-1, n, ans, v, i+1, sum+i);
+            v.pop_back();
+        }
+    }
+};
