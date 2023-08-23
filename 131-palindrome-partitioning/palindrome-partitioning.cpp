5cb7d447379c6762bb5af66f47f7d14a3ffd9ae7
Chetan Mittal <107939336+chetanmittal19@users.noreply.github.com>
2023-08-23 16:09:15 +0530

    Time: 129 ms (53.18%) | Memory: 59.6 MB (36.36%) - LeetSync

---
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> ans;
+        vector<string> v;
+        recursive(s, ans, v, 0);
+        return ans;
+    }
+
+    void recursive(string s, vector<vector<string>> &ans, vector<string> &v, int ind){
+        if(ind==s.size()){
+            ans.push_back(v);
+            return;
+        }
+        
+        for(int i=ind; i<=s.size(); i++){
+            if(isPalindrome(s, ind, i)){
+                v.push_back(s.substr(ind, i-ind+1));
+                recursive(s, ans, v, i+1);
+                v.pop_back();
+            }
+        }
+    }
+
+    bool isPalindrome(string s, int st, int en){
+        while(st<en){
+            if(s[st]!=s[en]) return false;
+            st++;
+            en--;
+        }
+        return true;
+    }
+};
