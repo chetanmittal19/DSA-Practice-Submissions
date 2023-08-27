14bb3ca453aaabddd35bd7eacdbf4c4974259c67
Chetan Mittal <107939336+chetanmittal19@users.noreply.github.com>
2023-08-27 16:56:59 +0530

    Time: 0 ms (100.00%) | Memory: 6.3 MB (90.89%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.cpp
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -9,13 +9,13 @@ public:
 
     void recursive(int k, int n, vector<vector<int>> &ans, vector<int> &v, int ind, int sum){
         if(sum>n) return;
-        if(sum==n && k==0){
-            ans.push_back(v);
+        if(k==0){
+            if(sum==n) ans.push_back(v);
             return;
         }
 
         for(int i=ind; i<=9; i++){
-            if(i>n) break;
+            if(i>n) break; // without this also code will work, but if the n is smaller that is between (1 to 9) it will save lot of recursive calls
             v.push_back(i);
             recursive(k-1, n, ans, v, i+1, sum+i);
             v.pop_back();
