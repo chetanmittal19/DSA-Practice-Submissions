bf4991e9b411f7270da16f76f7b776c3a1650fd4
Chetan Mittal <107939336+chetanmittal19@users.noreply.github.com>
2023-08-29 19:22:46 +0530

    Time: 140 ms (92.70%) | Memory: 10.3 MB (57.15%) - LeetSync

---
+++ 204-count-primes/count-primes.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        vector<bool> arr(n+1, true);
+        for(int i=2; i*i<=n; i++){
+            if(arr[i]){
+                for(int j=i*i; j<=n; j+=i) arr[j] = false;
+            }
+        }
+        int count = 0;
+        for(int i=2; i<n; i++){
+            if(arr[i]) count++;
+        }
+        return count;
+    }
+};
