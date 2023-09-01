abc86b1091faffbd29196e27b6330fa93fbf0f18
Chetan Mittal <107939336+chetanmittal19@users.noreply.github.com>
2023-09-01 13:37:52 +0530

    Time: 3 ms (89.82%) | Memory: 7.8 MB (89.73%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
+        //Method 1 O(N+1), O(1) -> ignoring the space for the answer
         vector<int> ans(n+1, 0);
         for(int i=1; i<=n; i++){
             ans[i] = (i&1) + ans[i/2];
