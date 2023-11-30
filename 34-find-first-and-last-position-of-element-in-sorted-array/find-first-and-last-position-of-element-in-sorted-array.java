69175be69f1da1d082db6b551365086b7cda10a0
Chetan Mittal <107939336+chetanmittal19@users.noreply.github.com>
2023-11-30 18:54:57 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (36.10%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -11,33 +11,64 @@ class Solution {
         return ans;
     }
 
+    // public int findFirst(int[] nums, int target){
+    //     int low = 0, high = nums.length-1;
+    //     while(low<=high){
+    //         int mid = low + (high-low)/2;
+    //         if(nums[mid]>target){
+    //             high = mid - 1;
+    //         } else if(nums[mid]<target) low = mid+1;
+    //         else{
+    //             if(mid==0 || nums[mid]!=nums[mid-1]) return mid;
+    //             else high = mid-1;
+    //         }
+    //     }
+    //     return -1;
+    // }
+    
     public int findFirst(int[] nums, int target){
         int low = 0, high = nums.length-1;
+        int ans = -1;
         while(low<=high){
             int mid = low + (high-low)/2;
-            if(nums[mid]>target){
-                high = mid - 1;
-            } else if(nums[mid]<target) low = mid+1;
+            if(nums[mid]>target) high = mid - 1;
+            else if(nums[mid]<target) low = mid+1;
             else{
-                if(mid==0 || nums[mid]!=nums[mid-1]) return mid;
-                else high = mid-1;
+                ans = mid;
+                high = mid-1;
             }
         }
-        return -1;
+        return ans;
     }
+
+    // public int findLast(int[] nums, int target){
+    //     int low = 0, high = nums.length-1;
+    //     while(low<=high){
+    //         int mid = low + (high-low)/2;
+    //         if(nums[mid]>target){
+    //             high = mid - 1;
+    //         } else if(nums[mid]<target) low = mid+1;
+    //         else {
+    //             if(mid==nums.length-1 || nums[mid]!=nums[mid+1]) return mid;
+    //             else low = mid + 1;
+    //         }
+    //     }
+    //     return -1;
+    // }
 
     public int findLast(int[] nums, int target){
         int low = 0, high = nums.length-1;
+        int ans = -1;
         while(low<=high){
             int mid = low + (high-low)/2;
             if(nums[mid]>target){
                 high = mid - 1;
             } else if(nums[mid]<target) low = mid+1;
             else {
-                if(mid==nums.length-1 || nums[mid]!=nums[mid+1]) return mid;
-                else low = mid + 1;
+                ans = mid;
+                low = mid + 1;
             }
         }
-        return -1;
+        return ans;
     }
 }
