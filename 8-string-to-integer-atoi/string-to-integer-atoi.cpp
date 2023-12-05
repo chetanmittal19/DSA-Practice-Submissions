b595033240dccc8c0070240e3459d72354993f61
Chetan Mittal <107939336+chetanmittal19@users.noreply.github.com>
2023-12-06 02:24:31 +0530

    Time: 0 ms (100.00%) | Memory: 6.6 MB (68.81%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,36 +1,38 @@
 class Solution {
 public:
     int myAtoi(string s) {
+        
         // Method 1 [Iterative] O(N), O(1)
-        // if(s.size()==0) return 0;
+        if(s.size()==0) return 0;
 
-        // int i=0;
-        // while(i<s.size() && s[i]==' ') i++;
-        // s = s.substr(i);
+        int i=0;
+        while(i<s.size() && s[i]==' ') i++;
+        s = s.substr(i);
 
-        // int sign=1;
-        // if(s[0]=='-') sign = -1;
-        // i = (s[0]=='+' || s[0]=='-') ? 1 : 0;
+        int sign=1;
+        if(s[0]=='-') sign = -1;
+        i = (s[0]=='+' || s[0]=='-') ? 1 : 0;
 
-        // long ans=0;
-        // while(i<s.size()){
-        //     if(s[i]==' ' || !isdigit(s[i])) break;
-        //     ans = ans*10 + (s[i]-'0');
-        //     if(sign==-1 && -1 * ans < INT_MIN) return INT_MIN;
-        //     if(sign==1 && ans > INT_MAX) return INT_MAX;
-        //     i++;
-        // }
-        // return (int)(sign * ans);
+        long ans=0;
+        while(i<s.size()){
+            if(s[i]==' ' || !isdigit(s[i])) break;
+            ans = ans*10 + (s[i]-'0');
+            if(sign==-1 && -1 * ans < INT_MIN) return INT_MIN;
+            else if(sign==1 && ans > INT_MAX) return INT_MAX;
+            i++;
+        }
+        return (int)(sign * ans);
 
         // Method 2 [Recursive] O(N), O(N)
-        int i=0, n=s.size(), sign=1;
-        while(i<s.size() && s[i]==' ') i++;
-        if(s[i]=='+') i++;
-        else if(s[i]=='-'){
-            i++;
-            sign=-1;
-        }
-        return recursiveAtoi(s, i, sign, 0);
+        //removing trailing spaces first and then calling recursive function
+        // int i=0, n=s.size(), sign=1;
+        // while(i<s.size() && s[i]==' ') i++;
+        // if(s[i]=='+') i++;
+        // else if(s[i]=='-'){
+        //     i++;
+        //     sign=-1;
+        // }
+        // return recursiveAtoi(s, i, sign, 0);
     }
 
     int recursiveAtoi(string s, int i, int sign, long ans){
