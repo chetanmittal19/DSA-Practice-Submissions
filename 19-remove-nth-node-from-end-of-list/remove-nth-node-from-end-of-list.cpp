71cfcb84d372d23f66cfc5c2526489c6628bdb2b
Chetan Mittal <107939336+chetanmittal19@users.noreply.github.com>
2023-12-08 01:24:08 +0530

    Time: 3 ms (67.48%) | Memory: 10.9 MB (98.56%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -11,17 +11,45 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        if(head==NULL) return head;
-        ListNode *fast = head;
-        for(int i=0; i<n; i++) fast = fast->next;
-
-        if(fast==NULL) return head->next;
-        ListNode *slow = head;
-        while(fast->next!=NULL){
-            fast = fast->next;
-            slow = slow->next;
+        // Method 1 [BruteForce]
+        ListNode* temp = head;
+        int cnt = 0;
+        while(temp!=NULL){
+            temp = temp-> next;
+            cnt++;
         }
-        slow->next = slow->next->next;
+        if(cnt==n){
+            ListNode* newHead = head->next;
+            head->next = NULL;
+            delete head;
+            return newHead;
+        }
+        int res = cnt-n; 
+        temp = head;
+        while(temp!=NULL){
+            res--;
+            if(res==0) break;
+            temp = temp->next;
+        }
+        // now temp is pointing to node before the node to be deleted
+        ListNode* delNode = temp->next;
+        temp->next = temp->next->next;
+        delNode->next = NULL;
+        delete delNode;
         return head;
+
+        // Method 2 [Optimised]
+        // if(head==NULL) return head;
+        // ListNode *fast = head;
+        // for(int i=0; i<n; i++) fast = fast->next;
+
+        // if(fast==NULL) return head->next; // It means n is equal to length of linkedlist
+        // ListNode *slow = head;
+        // while(fast->next!=NULL){
+        //     fast = fast->next;
+        //     slow = slow->next;
+        // }
+        // slow->next = slow->next->next;
+        // return head;
     }
 };
