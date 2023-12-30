e5c60bdab63c638af819f3f752724038312b5244
Chetan Mittal <107939336+chetanmittal19@users.noreply.github.com>
2023-12-30 17:48:07 +0530

    Time: 297 ms (7.22%) | Memory: 75.9 MB (91.58%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,27 +1,44 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
+        // Method 1 [BruteForce] O(N^2), O(1)
+
+        // Method 2 [Stack] O(2N), O(4N)
+        // int n = heights.size();
+        // stack<int> st1;
+        // stack<int> st2;
+        // int prevMin[n];
+        // int nextMin[n];
+        // for(int i=0; i<n; i++){
+        //     while(!st1.empty() && heights[st1.top()]>heights[i]) st1.pop(); // for duplicacy
+        //     prevMin[i] = st1.empty() ? -1 : st1.top();
+        //     st1.push(i);
+
+        //     while(!st2.empty() && heights[st2.top()]>=heights[n-i-1]) st2.pop(); // for duplicacy
+        //     nextMin[n-i-1] = st2.empty() ? n : st2.top();
+        //     st2.push(n-i-1);
+        // }
+
+        // int ans = 0;
+        // for(int i=0; i<n; i++){
+        //     int val = heights[i] * (nextMin[i] - prevMin[i] - 1);
+        //     ans = max(ans, val);
+        // }
+        // return ans;
+
+        // Method 3 [Stack, Striver] O(2N), O(1)
+        stack<int> st;
+        int maxA = 0;
         int n = heights.size();
-        stack<int> st1;
-        stack<int> st2;
-        int prevMin[n];
-        int nextMin[n];
-        for(int i=0; i<n; i++){
-            while(!st1.empty() && heights[st1.top()]>heights[i]) st1.pop(); // for duplicacy
-            prevMin[i] = st1.empty() ? -1 : st1.top();
-            st1.push(i);
-
-            while(!st2.empty() && heights[st2.top()]>=heights[n-i-1]) st2.pop(); // for duplicacy
-            nextMin[n-i-1] = st2.empty() ? n : st2.top();
-            st2.push(n-i-1);
+        for(int i=0; i<=n; i++){
+            while(!st.empty() && (i==n || heights[st.top()]>=heights[i])) {
+                int height = heights[st.top()];
+                st.pop();
+                int width = st.empty() ? i : i - st.top() - 1;
+                maxA = max(maxA, width * height);
+            }
+            st.push(i);
         }
-
-        int ans = 0;
-        for(int i=0; i<n; i++){
-            int val = heights[i] * (nextMin[i] - prevMin[i] - 1);
-            ans = max(ans, val);
-        }
-
-        return ans;
+        return maxA;
     }
 };
