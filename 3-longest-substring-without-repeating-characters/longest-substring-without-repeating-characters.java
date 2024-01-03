56dc94e3b975349b2c8d181f36449433ad32037d
Chetan Mittal <107939336+chetanmittal19@users.noreply.github.com>
2024-01-04 01:05:42 +0530

    Time: 6 ms (68.11%) | Memory: 45.3 MB (5.03%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -2,21 +2,32 @@ class Solution {
     public int lengthOfLongestSubstring(String s) {
         // Method 1 [BruteForce] O(N^3), O(1)
 
-        // Method 2 [Optimised] O(N^2), O(N)
+        // Method 2 [Better] O(N^2), O(N)
 
-        // Method 3 []
-
-        // Method 2 [HashMap]
-        int n = s.length();
-        HashMap<Character, Integer> map = new HashMap<>();
-        int left = 0, right = 0, maxLen = 0;
-        while(right<n) {
-            char ch = s.charAt(right);
-            if(map.containsKey(ch)) left = Math.max(left, map.get(ch)+1);
-            map.put(ch, right);
-            maxLen = Math.max(maxLen, right-left+1);
-            right++;
+        // Method 3 [Optimised, STRIVER] O(2N), O(N)
+        if(s.length()==0) return 0;
+        int maxans = Integer.MIN_VALUE;
+        Set <Character> set = new HashSet < > ();
+        int l = 0;
+        for (int r = 0; r < s.length(); r++) {
+            char ch = s.charAt(r);
+            while (l < r && set.contains(ch)) set.remove(s.charAt(l++));
+            set.add(ch);
+            maxans = Math.max(maxans, r - l + 1);
         }
-        return maxLen;
+        return maxans;
+
+        // Method 4 [HashMap, STRIVER] O(N), O(N)
+        // int n = s.length();
+        // HashMap<Character, Integer> map = new HashMap<>();
+        // int left = 0, right = 0, maxLen = 0;
+        // while(right<n) {
+        //     char ch = s.charAt(right);
+        //     if(map.containsKey(ch)) left = Math.max(left, map.get(ch)+1);
+        //     map.put(ch, right);
+        //     maxLen = Math.max(maxLen, right-left+1);
+        //     right++;
+        // }
+        // return maxLen;
     }
 }
