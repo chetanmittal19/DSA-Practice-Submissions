cdda0733109ccc15335885bd13bc1f70b8860d49
Chetan Mittal <107939336+chetanmittal19@users.noreply.github.com>
2024-01-14 01:47:40 +0530

    Time: 53 ms (77.29%) | Memory: 35 MB (59.44%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -1,22 +1,11 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        // vector<int> freq(26, 0);
-        // for(auto ch: tasks) freq[ch-'A']++;
-        // sort(tasks.begin(), tasks.end(), greater<int>());
-        // int idealSlots = (freq[0]-1)*n;
-        // for(int i=1; i<freq.size(); i++) idealSlots-= min(freq[0]-1, freq[i]);
-        // return tasks.size() + max(0, idealSlots);
-
-        vector<int>hash(26,0);
-        for(char c:tasks)hash[c-'A']++;
-        sort(hash.begin(),hash.end(),greater<int>());
-        int slots=hash[0]-1;
-        int idle=slots*n;
-        for(int i=1;i<hash.size();i++)
-        {
-            idle-=min(slots,hash[i]);
-        }
-        return tasks.size()+max(0,idle);
+        vector<int> freq(26, 0);
+        for(auto ch: tasks) freq[ch-'A']++;
+        sort(freq.begin(), freq.end(), greater<int>());
+        int idealSlots = (freq[0]-1)*n;
+        for(int i=1; i<freq.size(); i++) idealSlots-= min(freq[0]-1, freq[i]);
+        return tasks.size() + max(0, idealSlots);
     }
 };
