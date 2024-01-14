b7044c8eebb6627facad6778ec50cc34bfa4de8b
Chetan Mittal <107939336+chetanmittal19@users.noreply.github.com>
2024-01-14 18:21:58 +0530

    Time: 29 ms (75.93%) | Memory: 45.7 MB (19.54%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.java
+++ 876-hand-of-straights/hand-of-straights.java
@@ -1,15 +1,34 @@
 class Solution {
-    public boolean isNStraightHand(int[] hand, int n) {
-        if(hand.length%n!=0) return false;
+    public boolean isNStraightHand(int[] hand, int w) {
+        // Method 1 [PriorityQueue] O(2*NlogN), O(N)
+        // if(hand.length % w != 0) return false;
+        // PriorityQueue<Integer> pq = new PriorityQueue<>();
+        // for(int i: hand) pq.add(i);
+        // while(!pq.isEmpty()) {
+        //     int smallest = pq.poll();
+        //     for(int i=1; i<w; i++){
+        //         if(pq.remove(smallest+i)) continue; 
+        //         else return false;
+        //     }
+        // }
+        // return true;
 
+        if(hand.length % w != 0) return false;
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for(int i: hand) pq.add(i);
-
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int value: hand) map.put(value, map.getOrDefault(value, 0)+1);
+        for(int key: map.keySet()) pq.add(key); // adding only unique keys to pq
         while(!pq.isEmpty()) {
-            int smallest = pq.poll();
-            for(int i=1; i<n; i++){
-                if(pq.remove(smallest+i)) continue;
-                else return false;
+            int first = pq.peek();
+            for(int i = first; i<first+w; i++) {
+                if(!map.containsKey(i)) return false;
+                int count = map.get(i);
+                
+                // Occurances about to become 0 - > So remove
+                if(count==1){
+                    map.remove(i);
+                    pq.poll();
+                } else map.put(i, count-1);
             }
         }
         return true;
