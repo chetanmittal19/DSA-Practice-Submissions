3374a5228d03d85af3e1c5c0891689e2159bdf53
Chetan Mittal <107939336+chetanmittal19@users.noreply.github.com>
2024-03-04 02:26:53 +0530

    Time: 1 ms (19.07%) | Memory: 42.1 MB (40.79%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
@@ -17,40 +17,48 @@ class Solution {
     
     private TreeNode prev;
     public void flatten(TreeNode root) {
-        // Method 1 [Recursion] O(N)
-        if(root == null) return;
-        TreeNode dummy = new TreeNode(0);
-        prev = dummy;
-        flattenHelper(root);
-        prev.left = null;
+        // Method 1 [Recursion] O(n), O(h)
+        // if(root == null) return;
+        // TreeNode dummy = new TreeNode(0);
+        // prev = dummy;
+        // flattenHelper(root);
+        // prev.left = null;
 
-        // Method 2 [Iterative] O(N)
-        // if(root==null) return;
-        // Stack<TreeNode> st = new Stack<>();
-        // st.push(root);
-        // TreeNode temp = new TreeNode(0);
-        // TreeNode tail = temp;
-        // while(!st.isEmpty()) {
-        //     TreeNode front = st.pop();
-        //     while(front!=null) {
-        //         if(front.right!=null) st.push(front.right);
-        //         tail.right = front;
-        //         tail.left = null;
-        //         tail = tail.right;
-        //         front = front.left;
+        // Method 2 [Iterative, Stack] O(n), O(n)
+        if(root==null) return;
+        Stack<TreeNode> st = new Stack<>();
+        st.push(root);
+        while(!st.isEmpty()) {
+            TreeNode curr = st.pop();
+            if(curr.right!=null) st.push(curr.right);
+            if(curr.left!=null) st.push(curr.left);
+            if(!st.isEmpty()) curr.right = st.peek();
+            curr.left = null;
+        }
+
+        // Method 3 [Iterative, similar to morris traversal] O(2n), O(1)
+        // STRIVER: https://youtu.be/sWf7k1x9XR4
+        // TreeNode curr = root;
+        // while(curr!=null) {
+        //     if(curr.left!=null) {
+        //         TreeNode prev = curr.left;
+        //         while(prev.right!=null) prev = prev.right;
+        //         prev.right = curr.right;
+        //         curr.right = curr.left;
         //     }
+        //     curr.left = null;
+        //     curr = curr.right;
         // }
-
-        // tail.left = null;
+        // curr = root;
     }
 
-    private void flattenHelper(TreeNode root) {
-        if(root == null) return;
-        TreeNode rightSubTree = root.right;
-        prev.left = null;
-        prev.right = root;
-        prev = root;
-        flattenHelper(root.left);
-        flattenHelper(rightSubTree);
-    }
+    // private void flattenHelper(TreeNode root) {
+    //     if(root == null) return;
+    //     TreeNode rightSubTree = root.right;
+    //     prev.left = null;
+    //     prev.right = root;
+    //     prev = root;
+    //     flattenHelper(root.left);
+    //     flattenHelper(rightSubTree);
+    // }
 }
