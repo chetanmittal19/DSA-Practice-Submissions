d7403ac91e85fa1a5ab24612d6183c797e083de2
Chetan Mittal <107939336+chetanmittal19@users.noreply.github.com>
2024-03-04 01:38:48 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (47.61%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -21,21 +21,47 @@ class Solution {
         // inorder(root, res);
         // return res;
 
-        // Method 2 [Iterative] O(n), O(n)
-        List<Integer> res = new ArrayList<Integer>();
-        Stack<TreeNode> st = new Stack<>();
-        TreeNode current = root;
+        // Method 2 [Iterative] O(n), O(logn)
+        // List<Integer> res = new ArrayList<Integer>();
+        // Stack<TreeNode> st = new Stack<>();
+        // TreeNode current = root;
+        // while(current!=null || !st.isEmpty()) {
+        //     while(current!=null) {
+        //         st.push(current);
+        //         current = current.left;
+        //     }
+        //     current = st.pop();
+        //     res.add(current.val);
+        //     current = current.right;
+        // }
+        // return res;  
 
-        while(current!=null || !st.isEmpty()) {
-            while(current!=null) {
-                st.push(current);
-                current = current.left;
+        // Method 3 [Morris Traversal] O(2n), O(1)
+        List<Integer> res = new ArrayList<>();
+        TreeNode curr = root;
+        while(curr!=null) {
+            if(curr.left==null) {
+                res.add(curr.val);
+                curr = curr.right;
+            } else {
+                TreeNode pred = findPredecessor(curr);
+                if(pred.right==null) {
+                    pred.right = curr;
+                    curr = curr.left;
+                } else {
+                    res.add(curr.val);
+                    pred.right=null;
+                    curr = curr.right;
+                }
             }
-            current = st.pop();
-            res.add(current.val);
-            current = current.right;
         }
-        return res;  
+        return res;
+    }
+
+    private TreeNode findPredecessor(TreeNode curr) {
+        TreeNode t = curr.left;
+        while(t.right!=null && t.right!=curr) t = t.right;
+        return t;
     }
 
     // private void inorder(TreeNode root, List<Integer> res){
