1480d117c6683138be0a9e93a2f3f3cc2705f2bd
Chetan Mittal <107939336+chetanmittal19@users.noreply.github.com>
2024-03-05 17:22:59 +0530

    Time: 24 ms (43.02%) | Memory: 60.4 MB (50.87%) - LeetSync

--- 99-recover-binary-search-tree/recover-binary-search-tree.cpp
+++ 99-recover-binary-search-tree/recover-binary-search-tree.cpp
@@ -13,28 +13,48 @@ class Solution {
 public:
     void recoverTree(TreeNode* root) {
         // Method 1 [ArrayList] O(2n), O(n)
-        if(!root) return;
-        vector<TreeNode*> v;
-        getSortedElements(root, v);
-        TreeNode *prev = new TreeNode(INT_MIN);
-        TreeNode *first = NULL, *second = NULL;
-        for(int i=0; i<v.size(); i++) {
-            if(v[i]->val < prev->val) {
-                if(!first) first = prev;
-                second = v[i];
-            }
-            prev = v[i];
-        }
+        // if(!root) return;
+        // vector<TreeNode*> v;
+        // getSortedElements(root, v);
+        // TreeNode *prev = new TreeNode(INT_MIN);
+        // TreeNode *first = NULL, *second = NULL;
+        // for(int i=0; i<v.size(); i++) {
+        //     if(v[i]->val < prev->val) {
+        //         if(!first) first = prev;
+        //         second = v[i];
+        //     }
+        //     prev = v[i];
+        // }
+        // if(!first) return; // if tree is sorted, then first is always null
+        // int temp = first->val;
+        // first->val = second->val;
+        // second->val = temp;
+
+        // Method 2 [Recursion]
+
+        TreeNode *prev = NULL, *first = NULL, *second = NULL;
+        fixSwapped(root, prev, first, second);
         if(!first) return;
         int temp = first->val;
         first->val = second->val;
         second->val = temp;
     }
 
-    void getSortedElements(TreeNode *root, vector<TreeNode*> &v) {
+    void fixSwapped(TreeNode *root, TreeNode *&prev, TreeNode *&first, TreeNode *&second) {
         if(!root) return;
-        getSortedElements(root->left, v);
-        v.push_back(root);
-        getSortedElements(root->right, v);
+        fixSwapped(root->left, prev, first, second);
+        if(prev && root->val < prev->val) {
+            if(!first) first = prev;
+            second = root;
+        }
+        prev = root;
+        fixSwapped(root->right, prev, first, second);
     }
+
+    // void getSortedElements(TreeNode *root, vector<TreeNode*> &v) {
+    //     if(!root) return;
+    //     getSortedElements(root->left, v);
+    //     v.push_back(root);
+    //     getSortedElements(root->right, v);
+    // }
 };
