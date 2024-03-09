3e200054d4ce34fa35d78fa8f9b2247ebb62abe2
Chetan Mittal <107939336+chetanmittal19@users.noreply.github.com>
2024-03-10 01:02:44 +0530

    Time: 51 ms (65.33%) | Memory: 36.4 MB (24.26%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -2,42 +2,19 @@ class Solution {
 public:
     int numEnclaves(vector<vector<int>>& grid) {
         // Method 1 [DFS] O(2(n+m) + n*m*4 + n*m), O(n*m)
-        // int n = board.size(), m = board[0].size();
-        // vector<vector<int>> vis(n, vector<int>(m, 0));
-        // int delRow[] = {-1, 0, 1, 0};
-        // int delCol[] = {0, 1, 0, -1};
-
-        // for(int i=0; i<m; i++) {
-        //     if(board[0][i] == 'O' && !vis[0][i]) dfs(0, i, vis, board, delRow, delCol); // first row
-        //     if(board[n-1][i] == 'O' && !vis[n-1][i]) dfs(n-1, i, vis, board, delRow, delCol); // last row
-        // }
-
-        // for(int i=0; i<n; i++) {
-        //     if(board[i][0] == 'O' && !vis[i][0]) dfs(i, 0, vis, board, delRow, delCol); // first col
-        //     if(board[i][m-1] == 'O' && !vis[i][m-1]) dfs(i, m-1, vis, board, delRow, delCol); // last col
-        // }
-
-        // for(int i=0; i<n; i++) {
-        //     for(int j=0; j<m; j++) {
-        //         if(!vis[i][j] && board[i][j]=='O') board[i][j] = 'X';
-        //     }
-        // }
-        // return;
-
-        // Method 2 [BFS] O(2(n+m) + n*m*4 + n*m), O(n*m)
         int n = grid.size(), m = grid[0].size();
         vector<vector<int>> vis(n, vector<int>(m, 0));
         int delRow[] = {-1, 0, 1, 0};
         int delCol[] = {0, 1, 0, -1};
 
         for(int i=0; i<m; i++) {
-            if(grid[0][i] == 1 && !vis[0][i]) bfs(0, i, vis, grid, delRow, delCol); // first row
-            if(grid[n-1][i] == 1 && !vis[n-1][i]) bfs(n-1, i, vis, grid, delRow, delCol); // last row
+            if(grid[0][i] == 1 && !vis[0][i]) dfs(0, i, vis, grid, delRow, delCol); // first row
+            if(grid[n-1][i] == 1 && !vis[n-1][i]) dfs(n-1, i, vis, grid, delRow, delCol); // last row
         }
 
         for(int i=0; i<n; i++) {
-            if(grid[i][0] == 1 && !vis[i][0]) bfs(i, 0, vis, grid, delRow, delCol); // first col
-            if(grid[i][m-1] == 1 && !vis[i][m-1]) bfs(i, m-1, vis, grid, delRow, delCol); // last col
+            if(grid[i][0] == 1 && !vis[i][0]) dfs(i, 0, vis, grid, delRow, delCol); // first col
+            if(grid[i][m-1] == 1 && !vis[i][m-1]) dfs(i, m-1, vis, grid, delRow, delCol); // last col
         }
 
         int res = 0;
@@ -47,41 +24,65 @@ public:
             }
         }
         return res;
+
+        // Method 2 [BFS] O(2(n+m) + n*m*4 + n*m), O(n*m)
+        // int n = grid.size(), m = grid[0].size();
+        // vector<vector<int>> vis(n, vector<int>(m, 0));
+        // int delRow[] = {-1, 0, 1, 0};
+        // int delCol[] = {0, 1, 0, -1};
+
+        // for(int i=0; i<m; i++) {
+        //     if(grid[0][i] == 1 && !vis[0][i]) bfs(0, i, vis, grid, delRow, delCol); // first row
+        //     if(grid[n-1][i] == 1 && !vis[n-1][i]) bfs(n-1, i, vis, grid, delRow, delCol); // last row
+        // }
+
+        // for(int i=0; i<n; i++) {
+        //     if(grid[i][0] == 1 && !vis[i][0]) bfs(i, 0, vis, grid, delRow, delCol); // first col
+        //     if(grid[i][m-1] == 1 && !vis[i][m-1]) bfs(i, m-1, vis, grid, delRow, delCol); // last col
+        // }
+
+        // int res = 0;
+        // for(int i=0; i<n; i++) {
+        //     for(int j=0; j<m; j++) {
+        //         if(!vis[i][j] && grid[i][j]==1) res++;
+        //     }
+        // }
+        // return res;
     }
 
 private:
-    // void dfs(int row, int col, vector<vector<int>> &vis, vector<vector<char>>& board, int delRow[], int delCol[]) {
-    //     vis[row][col] = 1;
-    //     int n = board.size();
-    //     int m = board[0].size();
-
-    //     for(int i=0; i<4; i++) {
-    //         int nRow = row + delRow[i];
-    //         int nCol = col + delCol[i];
-    //         if(nRow>=0 && nRow<n && nCol>=0 && nCol<m && !vis[nRow][nCol] && board[nRow][nCol]=='O') {
-    //             dfs(nRow, nCol, vis, board, delRow, delCol);
-    //         }
-    //     }
-    // }
-
-    void bfs(int row, int col, vector<vector<int>> &vis, vector<vector<int>> &grid, int delRow[], int delCol[]) {
+    void dfs(int row, int col, vector<vector<int>> &vis, vector<vector<int>> &grid, int delRow[], int delCol[]) {
+        vis[row][col] = 1;
         int n = grid.size();
         int m = grid[0].size();
-        vis[row][col] = 1;
-        queue<pair<int, int>> q;
-        q.push({row, col});
 
-        while(!q.empty()) {
-            auto node = q.front();
-            q.pop();
-            for(int i=0; i<4; i++) {
-                int nRow = node.first + delRow[i];
-                int nCol = node.second + delCol[i];
-                if(nRow>=0 && nRow<n && nCol>=0 && nCol<m && !vis[nRow][nCol] && grid[nRow][nCol]==1) {
-                    vis[nRow][nCol] = 1;
-                    q.push({nRow, nCol});
-                }
+        for(int i=0; i<4; i++) {
+            int nRow = row + delRow[i];
+            int nCol = col + delCol[i];
+            if(nRow>=0 && nRow<n && nCol>=0 && nCol<m && !vis[nRow][nCol] && grid[nRow][nCol]==1) {
+                dfs(nRow, nCol, vis, grid, delRow, delCol);
             }
         }
     }
+
+    // void bfs(int row, int col, vector<vector<int>> &vis, vector<vector<int>> &grid, int delRow[], int delCol[]) {
+    //     int n = grid.size();
+    //     int m = grid[0].size();
+    //     vis[row][col] = 1;
+    //     queue<pair<int, int>> q;
+    //     q.push({row, col});
+
+    //     while(!q.empty()) {
+    //         auto node = q.front();
+    //         q.pop();
+    //         for(int i=0; i<4; i++) {
+    //             int nRow = node.first + delRow[i];
+    //             int nCol = node.second + delCol[i];
+    //             if(nRow>=0 && nRow<n && nCol>=0 && nCol<m && !vis[nRow][nCol] && grid[nRow][nCol]==1) {
+    //                 vis[nRow][nCol] = 1;
+    //                 q.push({nRow, nCol});
+    //             }
+    //         }
+    //     }
+    // }
 };
