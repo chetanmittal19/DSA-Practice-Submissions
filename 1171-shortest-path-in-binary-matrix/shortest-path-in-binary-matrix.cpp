68b9ac248a80f066b055f444119afc485c8b69cb
Chetan Mittal <107939336+chetanmittal19@users.noreply.github.com>
2024-03-17 19:04:50 +0530

    Time: 48 ms (58.30%) | Memory: 25 MB (49.40%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -7,8 +7,9 @@ public:
         if(n==1 && m==1 && grid[0][0]==0) return 1;
         if(grid[0][0]==1 || grid[n-1][m-1]==1) return -1;
         vector<vector<int>> dist(n, vector<int>(m, 1e9));
-        dist[0][0] = 0;
-        q.push({0, {0, 0}});
+
+        dist[0][0] = 1;
+        q.push({1, {0, 0}}); // {dis, {row, col}}
         int dr[] = {0, 0, 1, -1, 1, -1, 1, -1};
         int dc[] = {1, -1, 0, 0, 1, -1, -1, 1};
         while(!q.empty()) {
@@ -22,7 +23,7 @@ public:
                 int newC = c + dc[i];
                 if(newR>=0 && newR<n && newC>=0 && newC<m && grid[newR][newC]==0 && dis+1<dist[newR][newC]) {
                     dist[newR][newC] = dis+1;
-                    if(newR==n-1 && newC==m-1) return dist[newR][newC] + 1; // or return dis + 2
+                    if(newR==n-1 && newC==m-1) return dis+1;
                     q.push({1+dis, {newR, newC}});
                 }
             }
