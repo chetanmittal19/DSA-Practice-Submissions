89814e44da7dbeaf685b0a022321ea8049dad03d
Chetan Mittal <107939336+chetanmittal19@users.noreply.github.com>
2024-03-17 18:50:23 +0530

    Time: 44 ms (73.00%) | Memory: 24.9 MB (49.40%) - LeetSync

---
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        queue<pair<int, pair<int, int>>> q;
+        int n = grid.size();
+        int m = grid[0].size();
+        if(n==1 && m==1 && grid[0][0]==0) return 1;
+        if(grid[0][0]==1 || grid[n-1][m-1]==1) return -1;
+        vector<vector<int>> dist(n, vector<int>(m, 1e9));
+        dist[0][0] = 0;
+        q.push({0, {0, 0}});
+        int dr[] = {0, 0, 1, -1, 1, -1, 1, -1};
+        int dc[] = {1, -1, 0, 0, 1, -1, -1, 1};
+        while(!q.empty()) {
+            auto it = q.front();
+            q.pop();
+            int dis = it.first;
+            int r = it.second.first;
+            int c = it.second.second;
+            for(int i=0; i<8; i++) {
+                int newR = r + dr[i];
+                int newC = c + dc[i];
+                if(newR>=0 && newR<n && newC>=0 && newC<m && grid[newR][newC]==0 && dis+1<dist[newR][newC]) {
+                    dist[newR][newC] = dis+1;
+                    if(newR==n-1 && newC==m-1) return dist[newR][newC] + 1; // or return dis + 2
+                    q.push({1+dis, {newR, newC}});
+                }
+            }
+        }
+        return -1;
+    }
+};
