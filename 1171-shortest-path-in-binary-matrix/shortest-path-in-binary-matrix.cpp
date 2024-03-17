144f1b9ea38c6f4b3f1c40e047e739abc24449c2
Chetan Mittal <107939336+chetanmittal19@users.noreply.github.com>
2024-03-17 19:08:37 +0530

    Time: 49 ms (54.89%) | Memory: 24.8 MB (56.61%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -4,7 +4,7 @@ public:
         queue<pair<int, pair<int, int>>> q;
         int n = grid.size();
         int m = grid[0].size();
-        if(n==1 && m==1 && grid[0][0]==0) return 1;
+        // if(n==1 && m==1 && grid[0][0]==0) return 1;
         if(grid[0][0]==1 || grid[n-1][m-1]==1) return -1;
         vector<vector<int>> dist(n, vector<int>(m, 1e9));
 
@@ -18,12 +18,12 @@ public:
             int dis = it.first;
             int r = it.second.first;
             int c = it.second.second;
+            if(r==n-1 && c==m-1) return dis;
             for(int i=0; i<8; i++) {
                 int newR = r + dr[i];
                 int newC = c + dc[i];
                 if(newR>=0 && newR<n && newC>=0 && newC<m && grid[newR][newC]==0 && dis+1<dist[newR][newC]) {
                     dist[newR][newC] = dis+1;
-                    if(newR==n-1 && newC==m-1) return dis+1;
                     q.push({1+dis, {newR, newC}});
                 }
             }
