5c2f0747c9dc315e4fbd9b214da67f1c8a5fcf92
Chetan Mittal <107939336+chetanmittal19@users.noreply.github.com>
2024-03-20 19:19:00 +0530

    Time: 71 ms (11.12%) | Memory: 36 MB (71.11%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
@@ -1,77 +1,72 @@
 class Solution {
 public:
     int countPaths(int n, vector<vector<int>>& roads) {
-        vector<pair<int, int>> adj[n];
-        
-        for(auto it : roads)
-        {
-            adj[it[0]].push_back({it[1], it[2]});
-            adj[it[1]].push_back({it[0], it[2]});
-        }
-        
-        priority_queue<pair<long long, int>, vector<pair<long long, int>>,
-        greater<pair<long long, int>>> pq;
-        vector<long long> dist(n, 1e18);
-        vector<long long> ways(n, 0);
-        dist[0] = 0;
-        ways[0] = 1;
-        pq.push({0, 0});
-        int mod = (1e9 + 7);
-        
-        while(!pq.empty())
-        {
-            long long dis = pq.top().first;
-            int node = pq.top().second;
-            pq.pop();
-            
-            for(auto it : adj[node])
-            {
-                long long edgeWeight = it.second;
-                int adjNode = it.first;
-                
-                if(dis + edgeWeight < dist[adjNode])
-                {
-                    dist[adjNode] = dis + edgeWeight;
-                    pq.push({dist[adjNode], adjNode});
-                    ways[adjNode] = ways[node];
-                }
-                else if(dis + edgeWeight == dist[adjNode])
-                {
-                    ways[adjNode] = (ways[adjNode] + ways[node]) % mod;
-                }
-            }
-        }
-        return ways[n - 1] % mod;
         // vector<pair<int, int>> adj[n];
-        // for(auto it: roads) {
+        // for(auto it : roads) {
         //     adj[it[0]].push_back({it[1], it[2]});
         //     adj[it[1]].push_back({it[0], it[2]});
         // }
-
-        // priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> pq; // {dis, node}
-        // vector<long long> dist(n, 1e18), ways(n, 0);
+        
+        // priority_queue<pair<long long, int>, vector<pair<long long, int>>,
+        // greater<pair<long long, int>>> pq;
+        // vector<long long> dist(n, 1e18);
+        // vector<long long> ways(n, 0);
         // dist[0] = 0;
         // ways[0] = 1;
         // pq.push({0, 0});
-        // int mod = (int)(1e9 + 7);
-
+        // int mod = (1e9 + 7);
+        
         // while(!pq.empty()) {
-        //     int dis = pq.top().first;
+        //     long long dis = pq.top().first;
         //     int node = pq.top().second;
         //     pq.pop();
-
-        //     for(auto it: adj[node]) {
+            
+        //     for(auto it : adj[node]) {
+        //         long long edgeWeight = it.second;
         //         int adjNode = it.first;
-        //         int edW = it.second;
-        //         if(dis + edW < dist[adjNode]) {
-        //             dist[adjNode] = dis + edW;
-        //             pq.push({dis + edW, adjNode});
+                
+        //         if(dis + edgeWeight < dist[adjNode]) {
+        //             dist[adjNode] = dis + edgeWeight;
+        //             pq.push({dist[adjNode], adjNode});
         //             ways[adjNode] = ways[node];
-        //         } else if(dis + edW == dist[adjNode]) {
+        //         }
+        //         else if(dis + edgeWeight == dist[adjNode]) {
         //             ways[adjNode] = (ways[adjNode] + ways[node]) % mod;
         //         }
         //     }
         // }
-        // return ways[n-1] % mod;
+        // return ways[n - 1] % mod;
+
+        vector<pair<int, int>> adj[n];
+        for(auto it: roads) {
+            adj[it[0]].push_back({it[1], it[2]});
+            adj[it[1]].push_back({it[0], it[2]});
+        }
+
+        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> pq; // {dis, node}
+        vector<long long> dist(n, 1e18), ways(n, 0);
+        dist[0] = 0;
+        ways[0] = 1;
+        pq.push({0, 0});
+        int mod = (int)(1e9 + 7);
+
+        while(!pq.empty()) {
+            long long dis = pq.top().first;
+            int node = pq.top().second;
+            pq.pop();
+
+            for(auto it: adj[node]) {
+                int adjNode = it.first;
+                long long edW = it.second;
+                if(dis + edW < dist[adjNode]) {
+                    dist[adjNode] = dis + edW;
+                    pq.push({dis + edW, adjNode});
+                    ways[adjNode] = ways[node];
+                } else if(dis + edW == dist[adjNode]) {
+                    ways[adjNode] = (ways[adjNode] + ways[node]) % mod;
+                }
+            }
+        }
+        return ways[n-1] % mod;
     }
 };
