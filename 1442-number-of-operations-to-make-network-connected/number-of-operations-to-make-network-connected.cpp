b892ea8ad57bece6321b3fcd7a07cbb4b729e974
Chetan Mittal <107939336+chetanmittal19@users.noreply.github.com>
2024-03-23 01:34:23 +0530

    Time: 95 ms (42.70%) | Memory: 45.8 MB (59.91%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -1,22 +1,73 @@
+class DisJointSet
+{
+    public :
+    vector<int> rank;
+    vector<int> parent;
+     
+    DisJointSet(int n) {
+        rank.resize(n+1,0);
+        parent.resize(n+1);
+        int i;
+        for(i=0; i<=n; i++) parent[i]=i;
+    }
+
+    int findupar(int node) {
+        if(node==parent[node]) return node;
+        return parent[node] = findupar(parent[node]);
+    }
+
+    void unionbyrank(int u,int v) {
+        int pu=findupar(u);
+        int pv=findupar(v);
+        if(pu==pv) return;
+
+        if(rank[pu]<rank[pv]) parent[pu]=pv;
+        else if(rank[pv]<rank[pu]) parent[pv]=pu;
+        else {
+            parent[pu]=pv;
+            rank[pv]++;
+        }
+    }
+};
+
 class Solution {
 public:
     int makeConnected(int n, vector<vector<int>>& arr) {
-        int len = arr.size();
-        if(len<n-1) return -1;
-        vector<int> adj[n];
-        for(auto v : arr) {
-            adj[v[0]].push_back(v[1]);
-            adj[v[1]].push_back(v[0]);
+        // Method 1 [DisJoint DS]
+        DisJointSet ds(n);
+        int cnt=0;
+        for(auto it : arr) {
+            int u=it[0];
+            int v=it[1];
+            if(ds.findupar(u)!=ds.findupar(v)) ds.unionbyrank(u,v);
+            else cnt++;
         }
 
-        vector<bool> visited(n, false);
-        int ans = 0;
-        for(int i=0; i<n; i++)
-        if(!visited[i]) {
-            dfs(adj, visited, i);
-            ans++;
+        int nc=0;
+        int i;
+        for(i=0; i<n; i++) {
+            if(ds.parent[i]==i) nc++;
         }
-        return ans - 1;
+        if(cnt<nc-1) return -1;
+        return nc-1;
+
+        // Method 2 [DFS]
+        // int len = arr.size();
+        // if(len<n-1) return -1;
+        // vector<int> adj[n];
+        // for(auto v : arr) {
+        //     adj[v[0]].push_back(v[1]);
+        //     adj[v[1]].push_back(v[0]);
+        // }
+
+        // vector<bool> visited(n, false);
+        // int ans = 0;
+        // for(int i=0; i<n; i++)
+        // if(!visited[i]) {
+        //     dfs(adj, visited, i);
+        //     ans++;
+        // }
+        // return ans - 1;
     }
 
     void dfs(vector<int> adj[], vector<bool> &visited, int src) {
