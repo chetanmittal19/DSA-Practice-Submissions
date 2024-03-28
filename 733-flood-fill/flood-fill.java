47ea827eb7850b0e5ea8e78befe899f1e3ab258c
Chetan Mittal <107939336+chetanmittal19@users.noreply.github.com>
2024-03-28 18:29:23 +0530

    Time: 2 ms (12.28%) | Memory: 44.9 MB (45.64%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -9,8 +9,8 @@ class Solution {
     }
 
     public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        // Create a copy array in both method if orignal array modification is not allowed
         // Method 1 [DFS] O(n*m*4), O(n*m)->recursion
-        // In this solution we modified the orignal array but it is good practice to preserve orignal array and do modifications in copy array
         // int iniColor = image[sr][sc];
         // int[][] ans = image;
         // int[] delRow = {-1, 0, 1, 0};
@@ -18,18 +18,19 @@ class Solution {
         // dfs(sr, sc, ans, image, newColor, delRow, delCol, iniColor);
         // return ans;
 
-        // Method 2 [BFS]
+        // Method 2 [BFS] O(n*m*4), O(n*m)
         int iniColor = image[sr][sc];
         int n = image.length;
         int m = image[0].length;
         boolean[][] vis = new boolean[n][m];
         int[][] ans = image;
-        Queue<Pair> q = new LinkedList<>();
 
+        Queue<Pair> q = new LinkedList<>();
         q.add(new Pair(sr, sc));
         vis[sr][sc] = true;
         int[] delRow = {-1, 0, 1, 0};
         int[] delCol = {0, 1, 0, -1};
+
         while(!q.isEmpty()) {
             Pair p = q.poll();
             ans[p.sr][p.sc] = newColor;
@@ -45,16 +46,16 @@ class Solution {
         return ans;
     }
 
-    // private void dfs(int sr, int sc, int[][] ans, int[][] image, int newColor, int[] delRow, int[] delCol, int iniColor) {
-    //     ans[sr][sc] = newColor;
-    //     int n = image.length;
-    //     int m = image[0].length;
-    //     for(int i=0; i<4; i++) {
-    //         int nRow = sr + delRow[i];
-    //         int nCol = sc + delCol[i];
-    //         if(nRow>=0 && nRow<n && nCol>=0 && nCol<m && image[nRow][nCol]==iniColor && ans[nRow][nCol]!=newColor) {
-    //             dfs(nRow, nCol, ans, image, newColor, delRow, delCol, iniColor);
-    //         }
-    //     }
-    // }
+    private void dfs(int sr, int sc, int[][] ans, int[][] image, int newColor, int[] delRow, int[] delCol, int iniColor) {
+        ans[sr][sc] = newColor;
+        int n = image.length;
+        int m = image[0].length;
+        for(int i=0; i<4; i++) {
+            int nRow = sr + delRow[i];
+            int nCol = sc + delCol[i];
+            if(nRow>=0 && nRow<n && nCol>=0 && nCol<m && image[nRow][nCol]==iniColor) {
+                dfs(nRow, nCol, ans, image, newColor, delRow, delCol, iniColor);
+            }
+        }
+    }
 }
