0e14691694d6bdd0f77672b90a3340901837f502
Chetan Mittal <107939336+chetanmittal19@users.noreply.github.com>
2024-03-29 16:28:15 +0530

    Time: 19 ms (45.32%) | Memory: 16.2 MB (50.91%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -2,13 +2,24 @@ class Solution {
 public:
     bool isBipartite(vector<vector<int>>& graph) {
         // Method 1 [DFS] O(v + 2E), O(v)
+        // can also be done with BFS, i think
+        // int v = graph.size();
+        // int color[v];
+        // for(int i=0; i<v; i++) color[i] = -1;
+        // for(int i=0; i<v; i++) {
+        //     if(color[i]==-1) {
+        //         if(dfs(i, 0, color, graph)==false) return false;
+        //     }
+        // }
+        // return true;
+
+        // Method 2 [BFS] 
         int v = graph.size();
         int color[v];
         for(int i=0; i<v; i++) color[i] = -1;
-
         for(int i=0; i<v; i++) {
             if(color[i]==-1) {
-                if(dfs(i, 0, color, graph)==false) return false;
+                if(bfs(i, 0, color, graph)==false) return false;
             }
         }
         return true;
@@ -21,6 +32,24 @@ private:
             if(color[it]==-1) {
                 if(dfs(it, !col, color, graph)==false) return false;
             } else if(color[it]==col) return false;
+        }
+        return true;
+    }
+
+    bool bfs(int node, int col, int color[], vector<vector<int>>& graph) {
+        color[node] = col;
+        queue<int> q;
+        q.push(node);
+        while(!q.empty()) {
+            int n= q.front();
+            int clr = color[n];
+            q.pop();
+            for(auto it: graph[n]) {
+                if(color[it]==-1) {
+                    color[it] = !clr;
+                    q.push(it);
+                } else if(color[it]==clr) return false;
+            }
         }
         return true;
     }
