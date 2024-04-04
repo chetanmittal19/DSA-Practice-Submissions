d88c6f92aff2d7c1206d60a68a25fdee57880234
Chetan Mittal <107939336+chetanmittal19@users.noreply.github.com>
2024-04-04 17:07:51 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (63.69%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -18,13 +18,24 @@ class Solution {
         // }
         // return dp[n];
 
-        // Method 3 [Memoisation] O()
-        if(n<=0) return 0;
-        int[] dp = new int[n+1];
-        Arrays.fill(dp, -1);
-        dp[0] = dp[1] = 1;
-        calculateWays(dp, n);
-        return dp[n];
+        // Method 3 [Memoisation] O(n), O(2n)
+        // if(n<0) return 0;
+        // int[] dp = new int[n+1];
+        // Arrays.fill(dp, -1);
+        // dp[0] = dp[1] = 1;
+        // calculateWays(dp, n);
+        // return dp[n];
+
+        // Method 4 [Optimised] O(n), O(1)
+        if(n<0) return 0;
+        if(n==0 || n==1) return 1;
+        int prev = 1, beforePrev = 1;
+        for(int i=2; i<=n; i++) {
+            int temp = prev + beforePrev;
+            beforePrev = prev;
+            prev = temp;
+        }
+        return prev;
     }
 
     private int calculateWays(int[] dp, int n) {
